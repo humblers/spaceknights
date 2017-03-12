9219e62ab939c89ac97a7a3fddf882dce96d4a11
daeilkim <lazywierdo@icloud.com>
2017-03-12 17:45:09 +0900

    add multiple shot

--- game/player/player.gd
+++ game/player/player.gd
@@ -55,19 +55,29 @@ func _process(delta):
 		elif Input.is_key_pressed(KEY_RIGHT) and not on_right_edge:
 			on_left_edge = false
 			translate(Vector3(speed * delta, 0, 0))
-		if Input.is_key_pressed(KEY_SPACE):
-			fire()
 
-func fire():
+		if Input.is_key_pressed(KEY_1):
+			fire()
+		elif Input.is_key_pressed(KEY_2):
+			fire(true)
+
+func fire(multiple = false):
 	if fire_timeout > 0:
 		return
 	fire_timeout = FIRE_INTERVAL
+	
+	if multiple:
+		create_bullet(forward.rotated(Vector3(0, 1, 0), deg2rad(30)).normalized())
+		create_bullet(forward.rotated(Vector3(0, 1, 0), deg2rad(-30)).normalized())
+	create_bullet(forward)
+
+func create_bullet(direction):
 	var bullet = preload('../bullet/bullet.tscn').instance()
 	bullet.set_global_transform(get_node("BulletFrom").get_global_transform().orthonormalized())
-	bullet.set_linear_velocity(forward * bullet_speed) 
+	bullet.set_linear_velocity(direction * bullet_speed) 
 	bullet.set_mass(bullet_mass)
 	get_node('../').add_child(bullet)
-
+	
 func reached_left_edge():
 	on_left_edge = true
 
