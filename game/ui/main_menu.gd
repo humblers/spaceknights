ad665dabdceb413544642f6e44cbd26f2c875c75
jayb <jayb.ephemeral@icloud.com>
2017-03-21 01:59:16 +0900

    1. 메인 ui에서 space key로 게임 시작가능하게 변경 2. player1 skill queue를 위한 ui 공간만 마련 3. queue는 만들었는데 아직 ui에 보이거나 실제로 queue를 활용하진 않음

--- game/ui/main_menu.gd
+++ game/ui/main_menu.gd
@@ -1,10 +1,24 @@
-
 extends Control
 
 func _ready():
 	get_node("blue_team").set_text("Blue Team : " + str(Globals.get("blue_result")) + " WIN")
 	get_node("red_team").set_text("Red Team : " + str(Globals.get("red_result")) + " WIN")
+	set_process_unhandled_input(true)
 	
 func _on_play_pressed():
-
 	get_tree().change_scene("res://main.tscn")
+
+func _unhandled_input(event):
+	if event.type == InputEvent.KEY and event.is_action_pressed("ui_start_game"):
+		_on_play_pressed()
+
+func _on_turret_fixed_pressed():
+	start.player1_skiil_queue.append(constants.TURRET_FIXED_TYPE)
+	update_skill_queue()
+
+func _on_turret_forward_pressed():
+	start.player1_skiil_queue.append(constants.TURRET_FORWARD_TYPE)
+	update_skill_queue()
+
+func update_skill_queue():
+	pass
--- game/ui/start.gd
+++ game/ui/start.gd
@@ -1,3 +1,8 @@
+extends Node
+
+export var player1_skiil_queue = []
+export var player2_skill_queue = []
+
 func _ready():
 	Globals.set("blue_result", 0)
 	Globals.set("red_result", 0)
