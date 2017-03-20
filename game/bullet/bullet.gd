e043f0de845e81b22ce93f468b8094eef45033c2
daeilkim <lazywierdo@icloud.com>
2017-03-20 21:36:47 +0900

    add hp first

--- game/bullet/bullet.gd
+++ game/bullet/bullet.gd
@@ -1,6 +1,8 @@
 extends RigidBody
 
+const HP_MAX = 1000
 export var damage = 1
+export var hp = 1
 var life_timer = 0
 var decay_time = 1
 var is_enemy = false
@@ -27,3 +29,15 @@ func _process(delta):
 func on_timeout_complete():
 	#pass
 	queue_free()
+
+
+func _on_Bullet_body_enter( body ):
+	if (body.is_in_group("enemy_Bullet") and not is_enemy) \
+	or (body.is_in_group("player_Bullet") and is_enemy):
+		take_damage(body.damage)
+		
+func take_damage(damage):
+	print ('damage ', damage, ' takes')
+	hp = clamp(hp - damage, 0, HP_MAX)
+	if hp <= 0:
+		queue_free()
