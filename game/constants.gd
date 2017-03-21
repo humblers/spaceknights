ff5030ec3c49bcde2bc19dfdbfb98b4751d8be34
jayb <jayb.ephemeral@icloud.com>
2017-03-21 23:05:23 +0900

    메인메뉴의 스킬 버튼이 동작한다. ㅠㅠ 1. 스킬버튼을 누르면 큐에 등록 2. 큐의 버튼을 누르면 큐에서 스킬 제거 3. 스킬 큐는 10개를 맥시멈으로 제한

--- game/constants.gd
+++ game/constants.gd
@@ -1,5 +1,6 @@
 extends Node
 
+const SKILL_QUEUE_LEN = 10
 const TURRET_FIXED_TYPE = 1
 const TURRET_FORWARD_TYPE = 2
 
--- game/ui/main_menu.gd
+++ game/ui/main_menu.gd
@@ -4,6 +4,7 @@ func _ready():
 	get_node("blue_team").set_text("Blue Team : " + str(Globals.get("blue_result")) + " WIN")
 	get_node("red_team").set_text("Red Team : " + str(Globals.get("red_result")) + " WIN")
 	set_process_unhandled_input(true)
+	update_skill_queue()
 	
 func _on_play_pressed():
 	get_tree().change_scene("res://main.tscn")
@@ -13,12 +14,31 @@ func _unhandled_input(event):
 		_on_play_pressed()
 
 func _on_turret_fixed_pressed():
-	start.player1_skiil_queue.append(constants.TURRET_FIXED_TYPE)
+	var queue = start.player1_skill_queue
+	if queue.size() >= constants.SKILL_QUEUE_LEN:
+		return
+	queue.append(constants.TURRET_FIXED_TYPE)
 	update_skill_queue()
 
 func _on_turret_forward_pressed():
-	start.player1_skiil_queue.append(constants.TURRET_FORWARD_TYPE)
+	var queue = start.player1_skill_queue
+	if queue.size() >= constants.SKILL_QUEUE_LEN:
+		return
+	queue.append(constants.TURRET_FORWARD_TYPE)
 	update_skill_queue()
 
+var current = []
 func update_skill_queue():
-	pass
+	for prev_button in current:
+		prev_button.queue_free()
+	current = []
+	var skill_tscn = preload("res://ui/skill_button.tscn")
+	for i in range(start.player1_skill_queue.size()):
+		var button = preload("res://ui/skill_button.tscn").instance()
+		var skill = start.player1_skill_queue[i]
+		if skill == constants.TURRET_FIXED_TYPE:
+			button.set_text("FIXED")
+		elif skill == constants.TURRET_FORWARD_TYPE:
+			button.set_text("FORWARD")
+		get_node("skill_panel/scroll/skill_queue").add_child(button)
+		current.append(button)
