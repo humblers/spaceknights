0f66022b3577a8bea872569989ea0572ac53b844
jayb <jayb.ephemeral@icloud.com>
2017-03-22 22:18:07 +0900

    ui 포커스와 상관없이 space 누르면 시작하게 수정

--- game/ui/main_menu.gd
+++ game/ui/main_menu.gd
@@ -3,13 +3,13 @@ extends Control
 func _ready():
 	get_node("blue_team").set_text("Blue Team : " + str(start.blue_score) + " WIN")
 	get_node("red_team").set_text("Red Team : " + str(start.red_score) + " WIN")
-	set_process_unhandled_input(true)
+	set_process_input(true)
 	update_skill_queue()
 	
 func _on_play_pressed():
 	get_tree().change_scene("res://main.tscn")
 
-func _unhandled_input(event):
+func _input(event):
 	if event.type == InputEvent.KEY and event.is_action_pressed("ui_start_game"):
 		_on_play_pressed()
 
