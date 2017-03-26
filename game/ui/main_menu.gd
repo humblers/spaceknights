49f7909d4b6310e6ef03c676520c3528630da43c
jayb <jayb.ephemeral@icloud.com>
2017-03-27 01:49:31 +0900

    스킬 저장 Globals 이용해서 구현

--- game/ui/main_menu.gd
+++ game/ui/main_menu.gd
@@ -3,27 +3,29 @@ extends Control
 func _ready():
 	get_node("blue_team").set_text("Blue Team : " + str(start.blue_score) + " WIN")
 	get_node("red_team").set_text("Red Team : " + str(start.red_score) + " WIN")
-	for button_name in ["picked_type_player1", "picked_type_player2"]:
-		initialize_knight_type_button(button_name)
+	for player in ["player1", "player2"]:
+		initialize_knight_type_button(player)
 	update_skill_queue()
 	set_process_input(true)
 
 func _on_play_pressed():
 	start.player1_type = get_node("picked_type_player1").get_selected_ID()
 	start.player2_type = get_node("picked_type_player2").get_selected_ID()
+	start.save_changes()
 	get_tree().change_scene("res://main.tscn")
 
 func _input(event):
 	if event.type == InputEvent.KEY and event.is_action_pressed("ui_start_game"):
 		_on_play_pressed()
 
-func initialize_knight_type_button(button_name):
-	var pick_button = get_node(button_name)
+func initialize_knight_type_button(player):
+	var pick_button = get_node("picked_type_%s" % player)
 	pick_button.clear()
 	var skill_len = constants.KNIGHTS.size()
 	pick_button.add_item("random", skill_len)
-	for i in range(skill_len):
+	for i in range(constants.KNIGHTS.size()):
 		pick_button.add_item(constants.KNIGHTS[i]["type"], i)
+	pick_button.select(start.get("%s_type" % player))
 
 func update_skill_panel(player):
 	var button_parent = get_node("picked_skill_%s/scroll/skill_queue" % player)
--- game/ui/start.gd
+++ game/ui/start.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var player1_skill_queue = []
-onready var player1_type = constants.KNIGHTS.size()
+var player1_type = constants.KNIGHTS.size()
 
 var player2_skill_queue = []
 onready var player2_type = constants.KNIGHTS.size()
@@ -15,4 +15,27 @@ onready var red_life = 0
 onready var gameover = false
 
 func _ready():
-	get_tree().change_scene("res://ui/main_menu.tscn")
+	player1_skill_queue += Globals.get("player1_skill_queue") \
+						if Globals.has("player1_skill_queue") else []
+	player1_type += Globals.get("player1_type") \
+				if Globals.has("player1_type") else -1
+	
+	player2_skill_queue += Globals.get("player2_skill_queue") \
+						if Globals.has("player2_skill_queue") else []
+	player2_type += Globals.get("player2_type") \
+				if Globals.has("player2_type") else -1
+	
+	save_changes()
+
+func set_persists_property_with_value(property):
+	Globals.set(property, get(property))
+	Globals.set_persisting(property, true)
+	
+func save_changes():
+	set_persists_property_with_value("player1_skill_queue")
+	set_persists_property_with_value("player1_type")
+	
+	set_persists_property_with_value("player2_skill_queue")
+	set_persists_property_with_value("player2_type")
+	
+	Globals.save()
