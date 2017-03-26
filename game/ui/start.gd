4f32ccd8a4917608dbd0411a3e243248760e6c2d
jayb <jayb.ephemeral@icloud.com>
2017-03-27 02:24:29 +0900

    1. 머지한 후에 깨진 기능들 복구 2. 아직도 Globals 사용 중

--- game/ui/start.gd
+++ game/ui/start.gd
@@ -1,10 +1,10 @@
 extends Node
 
 var player1_skill_queue = []
-var player1_type = constants.KNIGHTS.size()
+var player1_type = -1
 
 var player2_skill_queue = []
-onready var player2_type = constants.KNIGHTS.size()
+var player2_type = -1
 
 onready var blue_score = 0
 onready var red_score = 0
@@ -18,24 +18,26 @@ func _ready():
 	player1_skill_queue += Globals.get("player1_skill_queue") \
 						if Globals.has("player1_skill_queue") else []
 	player1_type += Globals.get("player1_type") \
-				if Globals.has("player1_type") else -1
+				if Globals.has("player1_type") else 0
 	
 	player2_skill_queue += Globals.get("player2_skill_queue") \
 						if Globals.has("player2_skill_queue") else []
 	player2_type += Globals.get("player2_type") \
-				if Globals.has("player2_type") else -1
+				if Globals.has("player2_type") else 0
 	
-	save_changes()
+	save_player_data()
 
-func set_persists_property_with_value(property):
-	Globals.set(property, get(property))
+func set_persists_property_with_value(property, value=null):
+	if value == null:
+		value = get(property)
+	Globals.set(property, value)
 	Globals.set_persisting(property, true)
 	
-func save_changes():
+func save_player_data(p1_button_idx=null, p2_button_idx=null):
 	set_persists_property_with_value("player1_skill_queue")
-	set_persists_property_with_value("player1_type")
+	set_persists_property_with_value("player1_type", p1_button_idx)
 	
 	set_persists_property_with_value("player2_skill_queue")
-	set_persists_property_with_value("player2_type")
+	set_persists_property_with_value("player2_type", p2_button_idx)
 	
 	Globals.save()
