d7b7327ef7ccd5474b723f88a67c01b8e4006cd6
hwangmaru <hwangmaru@gmail.com>
2017-03-26 18:39:20 +0900

    1. 규복이 리소스랑 병합.

--- game/player/player.gd
+++ game/player/player.gd
@@ -139,29 +139,100 @@ var is_hold_fire = false
 var laser
 
 func _ready():
-	var player = "player1" if not is_enemy else "player2"
-	knight_skill_queue = [] + start.get("%s_skill_queue" % player)
-	knight_num = start.get("%s_type" % player)
-
-	if knight_num == -1:
-		knight_num = randi() % constants.KNIGHTS.size()
-
-	var knight_info = constants.KNIGHTS[knight_num]
-	speed = knight_info["speed"]
-	fire_interval = knight_info["fire_interval"]
-	HP_MAX = knight_info["HP_MAX"]
-	skill_cool = knight_info["skill_cool"]
-	regen_cool = knight_info["regen_cool"]
-	bullet_type = knight_info["bullet"]["type"]
-	bullet_hp = knight_info["bullet"]["hp"]
-	bullet_speed = knight_info["bullet"]["speed"]
-	bullet_mass = knight_info["bullet"]["mass"]
-	bullet_scale = knight_info["bullet"]["scale"]
-	bullet_damage = knight_info["bullet"]["damage"]
-	bullet_decay_time= knight_info["bullet"]["decay_time"]
-	bullet_is_cannon = knight_info["bullet"]["is_cannon"]
-	bullet_is_mass = knight_info["bullet"]["is_mass"]
-
+	knight_skill_queue = [] + start.player1_skill_queue
+	
+	if knight_num == 0:
+		knight_num = randi() % knight_total_num + 1
+	if knight_num == 1:
+		speed = knight1_speed
+		fire_interval = knight1_fire_interval
+		HP_MAX = knight1_HP_MAX
+		skill_cool = knight1_skill_cool
+		regen_cool = knight1_regen_cool
+		bullet_type = knight1_bullet_type
+		bullet_hp = knight1_bullet_hp
+		bullet_speed = knight1_bullet_speed
+		bullet_mass = knight1_bullet_mass
+		bullet_scale = knight1_bullet_scale
+		bullet_damage = knight1_bullet_damage
+		bullet_decay_time= knight1_bullet_decay_time
+		bullet_is_cannon = knight1_bullet_is_cannon
+		bullet_is_mass = knight1_bullet_is_mass
+	elif knight_num == 2:
+		speed = knight2_speed
+		fire_interval = knight2_fire_interval
+		HP_MAX = knight2_HP_MAX
+		skill_cool = knight2_skill_cool
+		regen_cool = knight2_regen_cool
+		bullet_type = knight2_bullet_type
+		bullet_hp = knight2_bullet_hp
+		bullet_speed = knight2_bullet_speed
+		bullet_mass = knight2_bullet_mass
+		bullet_scale = knight2_bullet_scale
+		bullet_damage = knight2_bullet_damage
+		bullet_decay_time= knight2_bullet_decay_time
+		bullet_is_cannon = knight2_bullet_is_cannon
+		bullet_is_mass = knight2_bullet_is_mass
+	elif knight_num == 3:
+		speed = knight3_speed
+		fire_interval = knight3_fire_interval
+		HP_MAX = knight3_HP_MAX
+		skill_cool = knight3_skill_cool
+		regen_cool = knight3_regen_cool
+		bullet_type = knight3_bullet_type
+		bullet_hp = knight3_bullet_hp
+		bullet_speed = knight3_bullet_speed
+		bullet_mass = knight3_bullet_mass
+		bullet_scale = knight3_bullet_scale
+		bullet_damage = knight3_bullet_damage
+		bullet_decay_time= knight3_bullet_decay_time
+		bullet_is_cannon = knight3_bullet_is_cannon
+		bullet_is_mass = knight3_bullet_is_mass
+	elif knight_num == 4:
+		speed = knight4_speed
+		fire_interval = knight4_fire_interval
+		HP_MAX = knight4_HP_MAX
+		skill_cool = knight4_skill_cool
+		regen_cool = knight4_regen_cool
+		bullet_type = knight4_bullet_type
+		bullet_hp = knight4_bullet_hp
+		bullet_speed = knight4_bullet_speed
+		bullet_mass = knight4_bullet_mass
+		bullet_scale = knight4_bullet_scale
+		bullet_damage = knight4_bullet_damage
+		bullet_decay_time= knight4_bullet_decay_time
+		bullet_is_cannon = knight4_bullet_is_cannon
+		bullet_is_mass = knight4_bullet_is_mass
+	elif knight_num == 5:
+		speed = knight5_speed
+		fire_interval = knight5_fire_interval
+		HP_MAX = knight5_HP_MAX
+		skill_cool = knight5_skill_cool
+		regen_cool = knight5_regen_cool
+		bullet_type = knight5_bullet_type
+		bullet_hp = knight5_bullet_hp
+		bullet_speed = knight5_bullet_speed
+		bullet_mass = knight5_bullet_mass
+		bullet_scale = knight5_bullet_scale
+		bullet_damage = knight5_bullet_damage
+		bullet_decay_time= knight5_bullet_decay_time
+		bullet_is_cannon = knight5_bullet_is_cannon
+		bullet_is_mass = knight5_bullet_is_mass
+	elif knight_num == 6:
+		speed = knight6_speed
+		fire_interval = knight6_fire_interval
+		HP_MAX = knight6_HP_MAX
+		skill_cool = knight6_skill_cool
+		regen_cool = knight6_regen_cool
+		bullet_type = knight6_bullet_type
+		bullet_hp = knight6_bullet_hp
+		bullet_speed = knight6_bullet_speed
+		bullet_mass = knight6_bullet_mass
+		bullet_scale = knight6_bullet_scale
+		bullet_damage = knight6_bullet_damage
+		bullet_decay_time= knight6_bullet_decay_time
+		bullet_is_cannon = knight6_bullet_is_cannon
+		bullet_is_mass = knight6_bullet_is_mass
 	if is_enemy:
 		get_node("HP").set_pos(Vector2(10, 8))
 	else:
@@ -232,20 +303,11 @@ func _process(delta):
 			
 		translate(Vector3(speed * delta * enemy_moving_state, 0, 0))
 		if enemy_moving_state == 1:
-			if self.get_name() == 'Player':
-				self.get_node("MeshInstance").set_rotation_deg(Vector3(0,30,0))
-			elif self.get_name() == 'Enemy':
-				self.get_node("MeshInstance").set_rotation_deg(Vector3(0,0,-30))
+			self.get_node("MeshInstance").set_rotation_deg(Vector3(0,0,-30))
 		elif enemy_moving_state == -1:
-			if self.get_name() == 'Player':
-				self.get_node("MeshInstance").set_rotation_deg(Vector3(0,-30,0))
-			elif self.get_name() == 'Enemy':
-				self.get_node("MeshInstance").set_rotation_deg(Vector3(0,0,30))
+			self.get_node("MeshInstance").set_rotation_deg(Vector3(0,0,30))
 		else:
-			if self.get_name() == 'Player':
-				self.get_node("MeshInstance").set_rotation_deg(Vector3(0,180,0))
-			elif self.get_name() == 'Enemy':
-				self.get_node("MeshInstance").set_rotation_deg(Vector3(0,0,0))
+			self.get_node("MeshInstance").set_rotation_deg(Vector3(0,0,0))
 		activate_skill()
 		fire()
 	else:
@@ -253,23 +315,14 @@ func _process(delta):
 		
 		if Input.is_key_pressed(KEY_LEFT) and not on_left_edge:
 			on_right_edge = false
-			if self.get_name() == 'Player':
-				self.get_node("MeshInstance").set_rotation_deg(Vector3(0,-30,0))
-			elif self.get_name() == 'Enemy':
-				self.get_node("MeshInstance").set_rotation_deg(Vector3(0,0,30))
-			translate(Vector3(-speed * delta, 0, 0))			
+			self.get_node("MeshInstance").set_rotation_deg(Vector3(0,0,30))
+			translate(Vector3(-speed * delta, 0, 0))
 		elif Input.is_key_pressed(KEY_RIGHT) and not on_right_edge:
 			on_left_edge = false
 			translate(Vector3(speed * delta, 0, 0))
-			if self.get_name() == 'Player':
-				self.get_node("MeshInstance").set_rotation_deg(Vector3(0,30,0))
-			elif self.get_name() == 'Enemy':
-				self.get_node("MeshInstance").set_rotation_deg(Vector3(0,0,-30))
+			self.get_node("MeshInstance").set_rotation_deg(Vector3(0,0,-30))
 		else :
-			if self.get_name() == 'Player':
-				self.get_node("MeshInstance").set_rotation_deg(Vector3(0,-180,0))
-			elif self.get_name() == 'Enemy':
-				self.get_node("MeshInstance").set_rotation_deg(Vector3(0,0,0))
+			self.get_node("MeshInstance").set_rotation_deg(Vector3(0,0,0))
 
 		if Input.is_key_pressed(KEY_SPACE):
 			activate_skill()
@@ -344,7 +397,7 @@ func create_laser(direction):
 		get_node('../').add_child(laser)
 
 func activate_skill():
-	if is_ai && get_node('../').get_node('BlackHole'):
+	if is_ai && get_node('../').has_node('BlackHole'):
 		return
 	if knight_skill_queue.size() <= 0:
 		return
@@ -361,7 +414,7 @@ func activate_skill():
 		summon_blackhole()
 
 func summon_blackhole():
-	var blackhole = preload('../skills/blackhole.tscn').instance()
+	var blackhole = preload('../skills/blackhole/blackhole.tscn').instance()
 	blackhole.is_enemy = is_enemy
 	blackhole.set_global_transform(get_node("BulletFrom").get_global_transform().orthonormalized())
 	#blackhole.set_linear_velocity(forward * 10)
@@ -375,7 +428,7 @@ func summon_blackhole():
 	get_node('../').add_child(blackhole)
 
 func call_turret(type):
-	var turret = preload('../skills/turret.tscn').instance()
+	var turret = preload('../skills/turret/turret.tscn').instance()
 	turret.turret_type = type
 	turret.is_enemy = is_enemy
 	var mothership_node = get_node('../EnemyMothership') if is_enemy else get_node('../PlayerMothership')
@@ -385,7 +438,7 @@ func call_turret(type):
 	get_node('../').add_child(turret)
 
 func call_drone(type):
-	var drone = preload('../skills/drone.tscn').instance()
+	var drone = preload('../skills/drone/drone.tscn').instance()
 	drone.drone_type = type
 	drone.is_enemy = is_enemy
 	var mothership_node = get_node('../EnemyMothership') if is_enemy else get_node('../PlayerMothership')
--- game/ui/main_menu.gd
+++ game/ui/main_menu.gd
@@ -3,11 +3,9 @@ extends Control
 func _ready():
 	get_node("blue_team").set_text("Blue Team : " + str(start.blue_score) + " WIN")
 	get_node("red_team").set_text("Red Team : " + str(start.red_score) + " WIN")
-	for button_name in ["picked_type_player1", "picked_type_player2"]:
-		initialize_knight_type_button(button_name)
-	update_skill_queue()
 	set_process_input(true)
-
+	update_skill_queue()
+	
 func _on_play_pressed():
 	get_tree().change_scene("res://main.tscn")
 
@@ -15,31 +13,34 @@ func _input(event):
 	if event.type == InputEvent.KEY and event.is_action_pressed("ui_start_game"):
 		_on_play_pressed()
 
-func initialize_knight_type_button(button_name):
-	var pick_button = get_node(button_name)
-	pick_button.add_item("random", -1)
-	for i in range(constants.KNIGHTS.size()):
-		pick_button.add_item(constants.KNIGHTS[i]["type"], i)
+func _on_turret_fixed_pressed():
+	var queue = start.player1_skill_queue
+	if queue.size() >= constants.SKILL_QUEUE_LEN:
+		return
+	queue.append(constants.TURRET)
+	update_skill_queue()
 
-func update_skill_panel(player):
-	var button_parent = get_node("picked_skill_%s/scroll/skill_queue" % player)
+func _on_turret_forward_pressed():
+	var queue = start.player1_skill_queue
+	if queue.size() >= constants.SKILL_QUEUE_LEN:
+		return
+	queue.append(constants.DRONE)
+	update_skill_queue()
+
+func _on_blackhole_pressed():
+	var queue = start.player1_skill_queue
+	if queue.size() >= constants.SKILL_QUEUE_LEN:
+		return
+	queue.append(constants.BLACKHOLE)
+	update_skill_queue()
+
+func update_skill_queue():
+	var button_parent = get_node("skill_panel/scroll/skill_queue")
 	for prev_button in button_parent.get_children():
 		prev_button.queue_free()
 	var skill_tscn = preload("res://ui/skill_button.tscn")
-	for i in range(start.get("%s_skill_queue" % player).size()):
+	for i in range(start.player1_skill_queue.size()):
 		var button = skill_tscn.instance()
-		button.skill_type = start.get("%s_skill_queue" % player)[i]
+		button.skill_type = start.player1_skill_queue[i]
 		button.queue_idx = i
 		button_parent.add_child(button)
-
-func update_skill_queue(players=null):
-	if players == null:
-		players = ["player1", "player2"]
-	for player in players:
-		update_skill_panel(player)
-
-func _on_picked_type_player1_item_selected( ID ):
-	start.player1_type = ID
-
-func _on_picked_type_player2_item_selected( ID ):
-	start.player2_type = ID
