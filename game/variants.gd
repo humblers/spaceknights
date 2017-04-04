3c1e57788ef0036280dbccd47db0b2ad6e0df308
JayB <jayb.ephemeral@icloud.com>
2017-04-04 21:45:39 +0900

    using configfile instead of file. thx to daeilkim

--- game/variants.gd
+++ game/variants.gd
@@ -20,26 +20,18 @@ var red_life = 0
 var gameover = false
 
 func _ready():
-	var player_data = File.new()
-	if not player_data.file_exists("user://player_data.dat"):
-		return
-
-	var cur_line = {}
-	player_data.open("user://player_data.dat", File.READ)
-	while(not player_data.eof_reached()):
-		cur_line.parse_json(player_data.get_line())
-		for key in cur_line:
-			set(key, cur_line[key])
+	var player_data = ConfigFile.new()
+	var err = player_data.load("user://player_data.cfg")
+	for key in player_data.get_section_keys("preset_knights"):
+		preset_knights[key] = player_data.get_value("preset_knights", key)
 
 func update_preset_knight(key, preset):
 	preset_knights[key] = preset
 	save_player_data()
 
 func save_player_data():
-	var datas = {
-		"preset_knights" : preset_knights,
-	}
-	var player_data = File.new()
-	player_data.open("user://player_data.dat", File.WRITE)
-	player_data.store_line(datas.to_json())
-	player_data.close()
+	var player_data = ConfigFile.new()
+	for key in preset_knights:
+		player_data.set_value("preset_knights", key, preset_knights[key])
+	var err = player_data.save("user://player_data.cfg")
+	
