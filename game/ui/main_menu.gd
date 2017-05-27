69ba0841afc69cc55ac3561bd53b815227b88685
DESKTOP-H7DFNL9\maru <hwangmaru@gmail.com>
2017-05-27 17:10:32 +0900

    1. 이제 2p가 의미 없어져서 shop으로 대체 2. 코드적으론 2p 덱 불러오는게 사라져서 문제가 될 수 있으나, 서버에서 상대 덱을 불러오도록 해야함.

--- game/ui/main_menu.gd
+++ game/ui/main_menu.gd
@@ -3,17 +3,13 @@ extends Control
 func _ready():
 	get_node("Background/blue_team").set_text("Blue Team : " + str(variants.blue_score) + " WIN")
 	get_node("Background/red_team").set_text("Red Team : " + str(variants.red_score) + " WIN")
-	get_node("1P").hide()
-	get_node("2P").hide()
+	get_node("Deck").hide()
+	get_node("Shop").hide()
 	set_process_input(true)
 
 func _on_play_pressed():
-	for i in [1, 2]:
-		variants.set(
-			"player%d_knight" % i,
-			variants.preset_knights[get_node("%dP" % i).cur_preset["key"]]
-		)
-	
+	variants.set("player1_knight",variants.preset_knights[get_node("Deck").cur_preset["key"]])
+
 	print(variants.player1_knight)
 	print(variants.player2_knight)
 	get_tree().change_scene("res://main.tscn")
@@ -25,11 +21,11 @@ func _input(event):
 
 func _on_HButtonArray_button_selected( button_idx ):
 	if button_idx == 0:
-		get_node("1P").show()
-		get_node("2P").hide()
+		get_node("Deck").show()
+		get_node("Shop").hide()
 	elif button_idx == 1:
-		get_node("1P").hide()
-		get_node("2P").hide()
+		get_node("Deck").hide()
+		get_node("Shop").hide()
 	elif button_idx == 2:
-		get_node("1P").hide()
-		get_node("2P").show()
+		get_node("Deck").hide()
+		get_node("Shop").show()
