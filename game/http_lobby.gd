8b8ade720a6332fd6fec0f6e65624fae4a654192
JayB <jayb.ephemeral@icloud.com>
2017-07-03 22:51:45 +0900

    funcref replaced by signal. fix possible bug. remove org comment. that's too much -_-

--- game/http_lobby.gd
+++ game/http_lobby.gd
@@ -6,10 +6,11 @@ const LOBBY_PORT = 3333
 var http = HTTPClient.new()
 var timer = Timer.new()
 var req_queue = []
-var cur_req_callback
+var response_body = RawArray()
+var reserved_signal
 
 signal login_success(ret)
-signal failure(err_code, err_msg)
+#signal failure(err_code, err_msg)
 
 func _ready():
 	if not validate_conn() and not connect_to_lobby():
@@ -21,69 +22,69 @@ func _ready():
 	timer.connect("timeout", self, "_poll")
 	add_child(timer)
 	timer.start()
-	#request(HTTPClient.METHOD_GET, "/ping", {}, funcref(self, "callback"))
-	request(HTTPClient.METHOD_POST, "/login/dev", {"id":"1", "token":"temp"}, funcref(self, "callback"))
+	request(HTTPClient.METHOD_POST, "/login/dev", {"id":"1", "token":"temp"}, "login_success")
 
-func callback(ret):
+func emit_reserved_signal(ret):
 	print(ret)
+	if reserved_signal != null:
+		emit_signal(reserved_signal, ret)
+		reserved_signal = null
 
 func _poll():
+	# ongoing request poll and return
 	if http.get_status() == HTTPClient.STATUS_REQUESTING:
 		http.poll()
 		print("Requesting..")
 		return
+
+	# get response and return response by emit signal 
 	if (http.has_response()):
-		var headers = http.get_response_headers_as_dictionary() # Get response headers
-		print("code: ",http.get_response_code()) # Show response code
-		print("**headers:\\n",headers) # Show headers
-		
-		# Getting the HTTP Body
-		
+		var headers = http.get_response_headers_as_dictionary()
+		print("code: ",http.get_response_code())
+		print("**headers:\\n",headers)
+
 		if (http.is_response_chunked()):
-			# Does it use chunks?
 			print("Response is Chunked!")
 		else:
-			# Or just plain Content-Length
 			var bl = http.get_response_body_length()
 			print("Response Length: ",bl)
-				
-		# This method works for both anyway
-		
-		var rb = RawArray() # Array that will hold the data
-		
+
 		if http.get_status()==HTTPClient.STATUS_BODY:
 			http.poll()
-
-		var chunk = http.read_response_body_chunk() # Get a chunk
+		var chunk = http.read_response_body_chunk()
 		if (chunk.size()==0):
 			return
-		rb = rb + chunk # Append to read buffer
-		print("bytes got: ",rb.size())
-		var text = rb.get_string_from_utf8()
-		cur_req_callback.call_func(text)
-		cur_req_callback = null
+
+		response_body = response_body + chunk
+		print("bytes got: ",response_body.size())
+		var text = response_body.get_string_from_utf8()
+		var ret = {}
+		ret.parse_json(text)
+		response_body = RawArray()
+		emit_reserved_signal(ret)
+
+	var next_request = req_queue.front()
+	if next_request == null:
 		return
-	if req_queue.size() <= 0:
-		return
-	var request = req_queue[0]
 	req_queue.pop_front()
-	cur_req_callback = request[3]
 	# Some headers
 	var headers=[
 		"User-Agent: Pirulo/1.0 (Godot)",
 		"Accept: */*",
 	]
-	var method = request[0]
+	var method = next_request[0]
+	var url = next_request[1]
 	var body = ""
 	if method == HTTPClient.METHOD_POST:
-		body = request[2].to_json()
-		headers.append("Content-Type: application/json")
-		headers.append("Content-Length: %d" % body.length())
-	var err = http.request(request[0], request[1], headers, body)
+		body = next_request[2].to_json()
+		headers = headers + [
+			"Content-Type: application/json",
+			"Content-Length: %d" % body.length(),
+		]
+	reserved_signal = next_request[3]
+	var err = http.request(method, url, headers, body)
 	if err != OK:
-		cur_req_callback.call_func("err")
-		cur_req_callback = null
-		return
+		emit_reserved_signal({"err_code":-999, "err_msg":"unknown"})
 
 func connect_to_lobby():
 	var err = http.connect(LOBBY_HOST, LOBBY_PORT)
