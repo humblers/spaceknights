065a68fb6224ea796e3345fa325b741aa03efa23
daeilkim <lazywierdo@icloud.com>
2017-07-09 22:02:36 +0900

    Show summon buttons on mouse release TODO : handle differently between ui/game input

--- game/game.gd
+++ game/game.gd
@@ -9,15 +9,22 @@ func _ready():
 	kcp.send({"SessionId": SESSION_ID})
 	kcp.connect("packet_received", self, "update_game_state")
 	input.connect("mouse_dragged", self, "send_player_input")
+	input.connect("mouse_pressed", self, "show_ui")
 
 func _exit_tree():
 	kcp._disconnect()
-
-func send_player_input(x):
-	kcp.send({ "Move" : x })
 	
 func update_game_state(dict):
 	var players = dict.Players
 	for id in players:
 		var player = players[id]
 		get_node(id + "/knight").set_pos(Vector2(player.Position, 0))
+
+func send_player_input(x):
+	kcp.send({ "Move" : x })
+
+func show_ui(b):
+	if b:
+		get_node("ui").hide()
+	else:
+		get_node("ui").show()
