09b46f231bc319522c21c3cc31438a1fa3c381c9
daeilkim <lazywierdo@icloud.com>
2017-07-09 22:02:36 +0900

    Show summon buttons on mouse release TODO : handle differently between ui/game input
    Former-commit-id: 0da53cf554526d7ef024e4bb4e43be50a5617e69

--- game/input.gd
+++ game/input.gd
@@ -2,6 +2,7 @@ extends Node
 
 var pressed = false
 
+signal mouse_pressed(b)
 signal mouse_dragged(x)
 
 func _ready():
@@ -10,6 +11,7 @@ func _ready():
 func _input(event):
 	if event.type == InputEvent.MOUSE_BUTTON:
 		pressed = event.pressed
+		emit_signal("mouse_pressed", pressed)
 	
 	if pressed and event.type == InputEvent.MOUSE_MOTION:
 		emit_signal("mouse_dragged", event.relative_pos.x)
