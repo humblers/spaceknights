d569cc36f4d1b97eff7b6099b4807107b9676903
JayB <jayb.ephemeral@icloud.com>
2017-09-25 20:31:19 +0900

    add missing file

---
+++ client/unit/animation.gd
@@ -0,0 +1,19 @@
+extends AnimatedSprite
+
+var animation
+var frame
+
+signal changed(animation, frame)
+
+func _ready():
+	animation = get_animation()
+	frame = get_frame()
+	set_process(true)
+
+func _process(delta):
+	var prev_anim = animation
+	var prev_frame = frame
+	animation = get_animation()
+	frame = get_frame()
+	if animation != prev_anim or frame != prev_frame:
+		emit_signal("changed", animation, frame)
