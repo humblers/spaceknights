86bce632fdcc6aee5d3b519ae4daf7b5f1e04ef5
JayB <jayb.ephemeral@icloud.com>
2018-01-30 17:06:29 +0900

    format lobby files

--- server/lobby/main.go
+++ server/lobby/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "net/http"
+	"flag"
+	"fmt"
+	"net/http"
 
-    "github.com/alexedwards/scs/session"
-    "github.com/alexedwards/scs/engine/memstore"
+	"github.com/alexedwards/scs/engine/memstore"
+	"github.com/alexedwards/scs/session"
 
-    "github.com/go-chi/chi"
-    "github.com/go-chi/docgen"
-    "github.com/go-chi/chi/middleware"
-    "github.com/go-chi/render"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/docgen"
+	"github.com/go-chi/render"
 )
 
 var routes = flag.Bool("routes", false, "Generate router documentation")
@@ -19,84 +19,84 @@ var routes = flag.Bool("routes", false, "Generate router documentation")
 var store *memstore.MemStore = memstore.New(0)
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    r := chi.NewRouter()
+	r := chi.NewRouter()
 
-    r.Use(middleware.RequestID)
-    r.Use(middleware.Logger)
-    r.Use(middleware.Recoverer)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
-    r.Use(session.Manage(store))
+	r.Use(session.Manage(store))
 
-    r.Use(render.SetContentType(render.ContentTypeJSON))
+	r.Use(render.SetContentType(render.ContentTypeJSON))
 
-    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("root."))
-    })
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("root."))
+	})
 
-    r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("pong"))
-    })
+	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
 
-    r.Get("/panic", func(w http.ResponseWriter, r *http.Request) {
-        panic("test")
-    })
+	r.Get("/panic", func(w http.ResponseWriter, r *http.Request) {
+		panic("test")
+	})
 
-    r.Mount("/login", LoginRouter())
-    r.Mount("/match", MatchRouter())
-    r.Post("/logout", Logout)
+	r.Mount("/login", LoginRouter())
+	r.Mount("/match", MatchRouter())
+	r.Post("/logout", Logout)
 
-    // Passing -routes to the program will generate docs for the above
-    // router definition. See the `routes.json` file in this folder for
-    // the output.
-    if *routes {
-        fmt.Println(docgen.JSONRoutesDoc(r))
-        fmt.Println(docgen.MarkdownRoutesDoc(r, docgen.MarkdownOpts{
-            ProjectPath: "github.com/go-chi/chi",
-            Intro:       "Welcome to the chi/_examples/rest generated docs.",
-        }))
-        return
-    }
+	// Passing -routes to the program will generate docs for the above
+	// router definition. See the `routes.json` file in this folder for
+	// the output.
+	if *routes {
+		fmt.Println(docgen.JSONRoutesDoc(r))
+		fmt.Println(docgen.MarkdownRoutesDoc(r, docgen.MarkdownOpts{
+			ProjectPath: "github.com/go-chi/chi",
+			Intro:       "Welcome to the chi/_examples/rest generated docs.",
+		}))
+		return
+	}
 
-    http.ListenAndServe(":3333", r)
+	http.ListenAndServe(":3333", r)
 }
 
 // This is entirely optional, but I wanted to demonstrate how you could easily
 // add your own logic to the render.Respond method.
 func init() {
-    render.Respond = func(w http.ResponseWriter, r *http.Request, v interface{}) {
-        if err, ok := v.(error); ok {
+	render.Respond = func(w http.ResponseWriter, r *http.Request, v interface{}) {
+		if err, ok := v.(error); ok {
 
-            // We set a default error status response code if one hasn't been set.
-            if _, ok := r.Context().Value(render.StatusCtxKey).(int); !ok {
-                w.WriteHeader(400)
-            }
+			// We set a default error status response code if one hasn't been set.
+			if _, ok := r.Context().Value(render.StatusCtxKey).(int); !ok {
+				w.WriteHeader(400)
+			}
 
-            // We log the error
-            fmt.Printf("Logging err: %s\n", err.Error())
+			// We log the error
+			fmt.Printf("Logging err: %s\n", err.Error())
 
-            // We change the response to not reveal the actual error message,
-            // instead we can transform the message something more friendly or mapped
-            // to some code / language, etc.
-            render.DefaultResponder(w, r, render.M{"status": "error"})
-            return
-        }
+			// We change the response to not reveal the actual error message,
+			// instead we can transform the message something more friendly or mapped
+			// to some code / language, etc.
+			render.DefaultResponder(w, r, render.M{"status": "error"})
+			return
+		}
 
-        render.DefaultResponder(w, r, v)
-    }
+		render.DefaultResponder(w, r, v)
+	}
 }
-
 
 type CommonSuccess struct {
 }
+
 func (rd *CommonSuccess) Render(w http.ResponseWriter, r *http.Request) error {
-    return nil
+	return nil
 }
 
 func Logout(w http.ResponseWriter, r *http.Request) {
-    session.Clear(r)
-    render.Render(w, r, &CommonSuccess{})
+	session.Clear(r)
+	render.Render(w, r, &CommonSuccess{})
 }
 
 //--
@@ -109,35 +109,35 @@ func Logout(w http.ResponseWriter, r *http.Request) {
 // helps reveal information on the error, setting it on Err, and in the Render()
 // method, using it to set the application-specific error code in AppCode.
 type ErrResponse struct {
-    Err            error `json:"-"` // low-level runtime error
-    HTTPStatusCode int   `json:"-"` // http response status code
+	Err            error `json:"-"` // low-level runtime error
+	HTTPStatusCode int   `json:"-"` // http response status code
 
-    StatusText string `json:"status"`          // user-level status message
-    AppCode    int64  `json:"code,omitempty"`  // application-specific error code
-    ErrorText  string `json:"error,omitempty"` // application-level error message, for debugging
+	StatusText string `json:"status"`          // user-level status message
+	AppCode    int64  `json:"code,omitempty"`  // application-specific error code
+	ErrorText  string `json:"error,omitempty"` // application-level error message, for debugging
 }
 
 func (e *ErrResponse) Render(w http.ResponseWriter, r *http.Request) error {
-    render.Status(r, e.HTTPStatusCode)
-    return nil
+	render.Status(r, e.HTTPStatusCode)
+	return nil
 }
 
 func ErrInvalidRequest(err error) render.Renderer {
-    return &ErrResponse{
-        Err:            err,
-        HTTPStatusCode: 400,
-        StatusText:     "Invalid request.",
-        ErrorText:      err.Error(),
-    }
+	return &ErrResponse{
+		Err:            err,
+		HTTPStatusCode: 400,
+		StatusText:     "Invalid request.",
+		ErrorText:      err.Error(),
+	}
 }
 
 func ErrRender(err error) render.Renderer {
-    return &ErrResponse{
-        Err:            err,
-        HTTPStatusCode: 422,
-        StatusText:     "Error rendering response.",
-        ErrorText:      err.Error(),
-    }
+	return &ErrResponse{
+		Err:            err,
+		HTTPStatusCode: 422,
+		StatusText:     "Error rendering response.",
+		ErrorText:      err.Error(),
+	}
 }
 
 var ErrNotFound = &ErrResponse{HTTPStatusCode: 404, StatusText: "Resource not found."}
--- server/lobby/match.go
+++ server/lobby/match.go
@@ -1,196 +1,199 @@
 package main
 
 import (
-    "bufio"
-    "net"
-    "net/http"
-    "strconv"
-    "time"
+	"bufio"
+	"net"
+	"net/http"
+	"strconv"
+	"time"
 
-    "github.com/go-chi/chi"
-    "github.com/go-chi/render"
-    "github.com/alexedwards/scs/session"
-    "github.com/golang/glog"
+	"github.com/alexedwards/scs/session"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/render"
+	"github.com/golang/glog"
 )
 
 type Deck []string
 
 type Candidate struct {
-    UserId  string
-    Deck    *Deck
-    Knights *[]string
+	UserId  string
+	Deck    *Deck
+	Knights *[]string
 }
 
 type Find struct {
-    UserId     string
-    FindResult chan *Game
+	UserId     string
+	FindResult chan *Game
 }
 
 type MatchManager struct {
-    Candidacy  chan *Candidate
-    Withdraw   chan string
-    FindGame   chan *Find
-    Candidates map[string]*Candidate
-    Games      map[string]*Game
+	Candidacy  chan *Candidate
+	Withdraw   chan string
+	FindGame   chan *Find
+	Candidates map[string]*Candidate
+	Games      map[string]*Game
 }
 
 func (m *MatchManager) Run() {
-    for {
-        select {
-        case candidate := <- m.Candidacy:
-            m.Candidates[candidate.UserId] = candidate
-            if len(m.Candidates) < 2 {
-                break
-            }
-            m.MatchingCandidates()
-        case id := <- m.Withdraw:
-            delete(m.Candidates, id)
-        case find := <- m.FindGame:
-            game, exists := m.Games[find.UserId]
-            if exists && !m.IsGameRunning(game) {
-                delete(m.Games, find.UserId)
-                game = nil
-            }
-            find.FindResult <- game
-        }
-    }
+	for {
+		select {
+		case candidate := <-m.Candidacy:
+			m.Candidates[candidate.UserId] = candidate
+			if len(m.Candidates) < 2 {
+				break
+			}
+			m.MatchingCandidates()
+		case id := <-m.Withdraw:
+			delete(m.Candidates, id)
+		case find := <-m.FindGame:
+			game, exists := m.Games[find.UserId]
+			if exists && !m.IsGameRunning(game) {
+				delete(m.Games, find.UserId)
+				game = nil
+			}
+			find.FindResult <- game
+		}
+	}
 }
 
 func (m *MatchManager) MatchingCandidates() {
-    keys := make([]string, 0, 2)
-    for key := range m.Candidates {
-        keys = append(keys, key)
-        if len(keys) >= 2{
-            break
-        }
-    }
-    id1, id2 := keys[0], keys[1]
-    c1, c2 := m.Candidates[id1], m.Candidates[id2]
-    session := &Game{
-        //Host:      "13.125.74.237",
+	keys := make([]string, 0, 2)
+	for key := range m.Candidates {
+		keys = append(keys, key)
+		if len(keys) >= 2 {
+			break
+		}
+	}
+	id1, id2 := keys[0], keys[1]
+	c1, c2 := m.Candidates[id1], m.Candidates[id2]
+	session := &Game{
+		//Host:      "13.125.74.237",
 		//Host:      "127.0.0.1",
 		Host:      "192.168.1.3",
-        SessionID: strconv.FormatInt(time.Now().Unix(), 10),
-    }
-    m.Games[id1] = session; m.Games[id2] = session
-    delete(m.Candidates, id1); delete(m.Candidates, id2)
+		SessionID: strconv.FormatInt(time.Now().Unix(), 10),
+	}
+	m.Games[id1] = session
+	m.Games[id2] = session
+	delete(m.Candidates, id1)
+	delete(m.Candidates, id2)
 
-    conn, err := net.Dial("tcp", "127.0.0.1:9989")
-    if err != nil {
-        glog.Errorf("game server connect fail:%v", err)
-        return
-    }
-    defer conn.Close()
-    conn.Write(NewPacket(SessionRequest{
-        SessionId: session.SessionID,
-        Home: *c1,
-        Visitor: *c2,
-    }))
-    var created bool
-    if b, _, err := bufio.NewReader(conn).ReadLine(); err == nil {
-        packet := Packet(b)
-        var resp SessionResponse
-        if err := packet.Parse(&resp); err == nil {
-            created = resp.Created
-            glog.Infof("game session(%v, %v) create req result : %v", session.Host, session.SessionID, created)
-        }
-    }
-    if !created {
-        delete(m.Games, id1); delete(m.Games, id2)
-    }
+	conn, err := net.Dial("tcp", "127.0.0.1:9989")
+	if err != nil {
+		glog.Errorf("game server connect fail:%v", err)
+		return
+	}
+	defer conn.Close()
+	conn.Write(NewPacket(SessionRequest{
+		SessionId: session.SessionID,
+		Home:      *c1,
+		Visitor:   *c2,
+	}))
+	var created bool
+	if b, _, err := bufio.NewReader(conn).ReadLine(); err == nil {
+		packet := Packet(b)
+		var resp SessionResponse
+		if err := packet.Parse(&resp); err == nil {
+			created = resp.Created
+			glog.Infof("game session(%v, %v) create req result : %v", session.Host, session.SessionID, created)
+		}
+	}
+	if !created {
+		delete(m.Games, id1)
+		delete(m.Games, id2)
+	}
 }
 
-func (m *MatchManager) IsGameRunning(game *Game) (res bool){
-    conn, err := net.Dial("tcp", "127.0.0.1:9989")
-    if err != nil {
-        glog.Errorf("game server connect fail:%v", err)
-        return
-    }
-    defer conn.Close()
-    conn.Write(NewPacket(SessionRequest{
-        SessionId:      game.SessionID,
-        DoNotCreate:    true,
-    }))
-    if err := conn.SetReadDeadline(time.Now().Add(1 * time.Second)); err != nil {
-        glog.Errorf("read time out : %v", err)
-        return
-    }
-    b, _, err := bufio.NewReader(conn).ReadLine()
-    if err != nil {
-        return
-    }
-    packet := Packet(b)
-    var resp SessionResponse
-    if err := packet.Parse(&resp); err != nil {
-        return
-    }
-    return resp.Exists
+func (m *MatchManager) IsGameRunning(game *Game) (res bool) {
+	conn, err := net.Dial("tcp", "127.0.0.1:9989")
+	if err != nil {
+		glog.Errorf("game server connect fail:%v", err)
+		return
+	}
+	defer conn.Close()
+	conn.Write(NewPacket(SessionRequest{
+		SessionId:   game.SessionID,
+		DoNotCreate: true,
+	}))
+	if err := conn.SetReadDeadline(time.Now().Add(1 * time.Second)); err != nil {
+		glog.Errorf("read time out : %v", err)
+		return
+	}
+	b, _, err := bufio.NewReader(conn).ReadLine()
+	if err != nil {
+		return
+	}
+	packet := Packet(b)
+	var resp SessionResponse
+	if err := packet.Parse(&resp); err != nil {
+		return
+	}
+	return resp.Exists
 }
 
 var manager MatchManager
-func MatchRouter() chi.Router {
-    manager = MatchManager{
-        Candidacy:  make(chan *Candidate),
-        Withdraw:   make(chan string),
-        FindGame:   make(chan *Find),
-        Candidates: make(map[string]*Candidate),
-        Games:      make(map[string]*Game),
-    }
-    go manager.Run()
 
-    r := chi.NewRouter()
-    r.Post("/find", FindGame)
-    r.Post("/candidacy", Candidacy)
-    r.Post("/withdraw", WithDraw)
-    return r
+func MatchRouter() chi.Router {
+	manager = MatchManager{
+		Candidacy:  make(chan *Candidate),
+		Withdraw:   make(chan string),
+		FindGame:   make(chan *Find),
+		Candidates: make(map[string]*Candidate),
+		Games:      make(map[string]*Game),
+	}
+	go manager.Run()
+
+	r := chi.NewRouter()
+	r.Post("/find", FindGame)
+	r.Post("/candidacy", Candidacy)
+	r.Post("/withdraw", WithDraw)
+	return r
 }
 
 type CandidacyRequest struct {
-    Deck    Deck        `json:"deck"`
-    Knights []string    `json:"knights"`
+	Deck    Deck     `json:"deck"`
+	Knights []string `json:"knights"`
 }
 
 func (a *CandidacyRequest) Bind(r *http.Request) error {
-    return nil
+	return nil
 }
 
 func Candidacy(w http.ResponseWriter, r *http.Request) {
-    id, _ := session.GetString(r, "id")
-    data := &CandidacyRequest{}
-    if err := render.Bind(r, data); err != nil {
-        render.Render(w, r, ErrInvalidRequest(err))
-        return
-    }
-    manager.Candidacy <- &Candidate{
-        UserId:  id,
-        Deck:    &data.Deck,
-        Knights: &data.Knights,
-    }
-    render.Render(w, r, &CommonSuccess{})
+	id, _ := session.GetString(r, "id")
+	data := &CandidacyRequest{}
+	if err := render.Bind(r, data); err != nil {
+		render.Render(w, r, ErrInvalidRequest(err))
+		return
+	}
+	manager.Candidacy <- &Candidate{
+		UserId:  id,
+		Deck:    &data.Deck,
+		Knights: &data.Knights,
+	}
+	render.Render(w, r, &CommonSuccess{})
 }
 
 func WithDraw(w http.ResponseWriter, r *http.Request) {
-    id, _ := session.GetString(r, "id")
-    manager.Withdraw <- id
-    render.Render(w, r, &CommonSuccess{})
+	id, _ := session.GetString(r, "id")
+	manager.Withdraw <- id
+	render.Render(w, r, &CommonSuccess{})
 }
 
-
 func (rd *Game) Render(w http.ResponseWriter, r *http.Request) error {
-    return nil
+	return nil
 }
 
 func FindGame(w http.ResponseWriter, r *http.Request) {
-    id, _ := session.GetString(r, "id")
-    result := make(chan *Game)
-    manager.FindGame <- &Find{
-        UserId:     id,
-        FindResult: result,
-    }
-    game := <- result
-    if game != nil {
-        render.Render(w, r, game)
-        return
-    }
-    render.Render(w, r, ErrNotFound)
+	id, _ := session.GetString(r, "id")
+	result := make(chan *Game)
+	manager.FindGame <- &Find{
+		UserId:     id,
+		FindResult: result,
+	}
+	game := <-result
+	if game != nil {
+		render.Render(w, r, game)
+		return
+	}
+	render.Render(w, r, ErrNotFound)
 }
