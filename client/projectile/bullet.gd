6f8c6e15567a3908856146b08cc3a038f59c3c45
JayB <jayb.ephemeral@icloud.com>
2018-04-10 22:02:39 +0900

    knight change state when using spell, fix some size issue

--- client/projectile/bullet.gd
+++ client/projectile/bullet.gd
@@ -6,28 +6,28 @@ var lifetime
 var elapsed = 0
 
 func _ready():
-	set_fixed_process(true)
+	set_physics_process(true)
 
-func set_single_target(target, lifetime, position):
+func set_single_target(target, lifetime, position, parent):
 	self.lifetime = lifetime
-	set_pos(position)
-	set_z(global.LAYERS.Projectile)
-	target_position = target.get_pos()
-	target_radius = global.UNITS[target.name].radius
+	self.position = position
+	self.z_index = global.LAYERS.Projectile
+	target_position = parent.to_local(target.global_position)
+	target_radius = global.UNITS[target.u_name].radius
 	target.connect("position_changed", self, "update_target_position")
 
 func update_target_position(id, position):
-	target_position = position
+	target_position = get_parent().to_local(position)
 
-func _fixed_process(delta):
+func _physics_process(delta):
 	elapsed += delta
 	var remaining = lifetime - elapsed
 	if remaining <= 0:
 		queue_free()
 		return
 
-	var position = get_pos()
+	var position = self.position
 	var direction = (target_position - position).normalized()
 	var speed = (position.distance_to(target_position) - target_radius) / remaining
-	set_pos(position + direction * speed * delta)
-	set_rot(direction.angle())
+	self.position = position + direction * speed * delta
+	self.rotation = direction.angle() - PI / 2
--- client/unit/unit.gd
+++ client/unit/unit.gd
@@ -31,7 +31,7 @@ func initialize(unit):
 
 func set_position(pos):
 	self.position = pos * 3
-	emit_signal("position_changed", get_name(), pos)
+	emit_signal("position_changed", get_name(), self.global_position)
 
 func set_base():
 	if has_node("Base"):
@@ -69,7 +69,7 @@ func update_changes(unit):
 					global.UNITS[u_name].projectile,
 					target,
 					float(global.UNITS[u_name].prehitdelay + 1) / global.SERVER_UPDATES_PER_SECOND,
-					get_node("Body/Shotpoint").position)
+					get_node("Body/Shotpoint").global_position)
 	if unit.State == "frozen":
 		body.stop()
 		body.get_material().set_shader_param("frozen", true)
