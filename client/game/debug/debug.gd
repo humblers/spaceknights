4d68084efe3203e3919884051adea54f4f12aa11
JayB <jayb.ephemeral@icloud.com>
2018-08-28 20:33:48 +0900

    add unit status debugging scene
    fix debug key toggle bug
    
    remove garbage
    
    change path

---
+++ client/game/debug/debug.gd
@@ -0,0 +1,36 @@
+extends Node
+
+export var Hp = false
+
+export var Radius = false
+export var Radius_Color = Color(0, 1.0, 0, 1)
+
+export var AttackDamage = false
+
+export var AttackInterval = false
+
+export var AttackRange = false
+export var AttackRange_Color = Color(1.0, 1.0, 0, 1)
+
+func _unhandled_key_input(ev):
+	if not ev.pressed:
+		return
+	match ev.scancode:
+		KEY_F1:
+			Hp = not Hp
+		KEY_F2:
+			Radius = not Radius
+		KEY_F3:
+			AttackDamage = not AttackDamage
+		KEY_F4:
+			AttackInterval = not AttackInterval
+		KEY_F5:
+			AttackRange = not AttackRange
+
+func update(game):
+	for id in game.units:
+		var unit = game.units[id]
+		if not unit.has_node("debug"):
+			var debug_stat = preload("res://game/debug/unit.tscn").instance()
+			unit.add_child(debug_stat)
+		unit.get_node("debug").update(game, unit, self)
