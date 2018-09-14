72e17c1af268bcf07a6158fd2a70f172fd58aee3
DESKTOP-H7DFNL9\maru <hwangmaru@gmail.com>
2018-09-14 22:25:58 +0900

    blaster 유닛 추가 및 타 레인지 유닛 레인지 수정
    블래스터 유닛 추가
    블래스터 미사일 리소스 수정
    다른 레인지 유닛들 사거리 수정

---
+++ client/game/unit/blaster/missile.gd
@@ -0,0 +1,86 @@
+extends Node2D
+
+var targetId = 0
+var lifetime = 0
+var damage = 0
+var slowDuration = 0
+var game
+
+var anim
+var trajectory_initialized = false
+
+func Init(targetId, lifetime, damage, game):
+	self.targetId = targetId
+	self.lifetime = lifetime
+	self.damage = damage
+	self.game = game
+
+func Update():
+	if lifetime <= 0:
+		var target = game.FindUnit(targetId)
+		if target != null:
+			target.TakeDamage(damage, "Range")
+			target.MakeSlow(slowDuration)
+	lifetime -= 1
+
+func IsExpired():
+	return lifetime < 0
+
+func Destroy():
+	queue_free()
+
+func MakeFrozen(slowDuration):
+	self.slowDuration = slowDuration
+
+func _ready():
+	var dup = $AnimationPlayer.get_animation("attack").duplicate()
+	$AnimationPlayer.rename_animation("attack", "attack-ref")
+	$AnimationPlayer.add_animation("attack", dup)
+	$AnimationPlayer.play("attack")
+	anim = dup
+
+func _process(delta):
+	var target = game.FindUnit(targetId)
+	if target == null:
+		return
+	if not trajectory_initialized:
+		init_trajectory(target.global_position)
+		trajectory_initialized = true
+	update_hit_position(target.global_position)
+	rotation = PI/2 + (target.position - position).angle()
+
+func init_trajectory(hit_pos):
+	$HitPosition.global_position = hit_pos
+	for side in ["L", "R"]:
+		for i in range(4):
+			var path = "Missile%s%s:position" % [side, (i + 1)]
+			var degree_path = "Missile%s%s:rotation_degrees" % [side, (i + 1)]
+			var track_idx = anim.find_track(path)
+			var degree_track_idx = anim.find_track(degree_path)
+			var key_num = anim.track_get_key_count(track_idx)
+			var degree_key_num = anim.track_get_key_count(degree_track_idx)
+			
+			for j in key_num-4:
+				var r = (randi()%20+20)
+				var offset = randi()%3-1
+				var pos = $HitPosition.position * (j+1)/4
+				r = r*offset
+				var newpos = Vector2(anim.track_get_key_value(track_idx, j+4).x + r, pos.length()*-1)
+				anim.track_set_key_value(track_idx, j+4, newpos)
+				
+			for j in degree_key_num-3:
+				var ref_vec = Vector2(0, -1)
+				var degree = rad2deg(ref_vec.angle_to(anim.track_get_key_value(track_idx, j+4)- anim.track_get_key_value(track_idx, j+3)))
+				anim.track_set_key_value(degree_track_idx, j+3, degree)
+
+
+func update_hit_position(gpos):
+	$HitPosition.global_position = gpos
+	for side in ["L", "R"]:
+		for i in range(3):
+			var path = "Missile%s%s:position" % [side, (i + 1)]
+			var track_idx = anim.find_track(path)
+			var key_idx = anim.track_get_key_count(track_idx) - 1
+			anim.track_set_key_value(track_idx, key_idx, $HitPosition.position)
+
+		
---
+++ pkg/game/blaster.go
@@ -0,0 +1,94 @@
+package game
+
+import "github.com/humblers/spaceknights/pkg/fixed"
+
+type blaster struct {
+	*unit
+	targetId int
+	attack   int // elapsed time since attack start
+}
+
+func newBlaster(id int, level, posX, posY int, g Game, p Player) Unit {
+	return &blaster{
+		unit: newUnit(id, "blaster", p.Team(), level, posX, posY, g),
+	}
+}
+
+func (b *blaster) Update() {
+	b.SetVelocity(fixed.Vector{0, 0})
+	if b.freeze > 0 {
+		b.attack = 0
+		b.targetId = 0
+		b.freeze--
+		return
+	}
+	if b.attack > 0 {
+		b.handleAttack()
+	} else {
+		t := b.target()
+		if t == nil {
+			b.findTargetAndDoAction()
+		} else {
+			if b.withinRange(t) {
+				b.handleAttack()
+			} else {
+				b.findTargetAndDoAction()
+			}
+		}
+	}
+}
+
+func (b *blaster) target() Unit {
+	return b.game.FindUnit(b.targetId)
+}
+
+func (b *blaster) setTarget(u Unit) {
+	if u == nil {
+		b.targetId = 0
+	} else {
+		b.targetId = u.Id()
+	}
+}
+
+func (bl *blaster) fire() {
+	b := newBullet(bl.targetId, bl.bulletLifeTime(), bl.attackDamage(), bl.game)
+	bl.game.AddBullet(b)
+}
+
+func (b *blaster) findTargetAndDoAction() {
+	t := b.findTarget()
+	b.setTarget(t)
+	if t != nil {
+		if b.withinRange(t) {
+			b.handleAttack()
+		} else {
+			b.moveTo(t)
+		}
+	}
+}
+
+func (b *blaster) moveTo(u Unit) {
+	corner := b.game.Map().FindNextCornerInPath(
+		b.Position(),
+		u.Position(),
+		b.Radius(),
+	)
+	direction := corner.Sub(b.Position()).Normalized()
+	b.SetVelocity(direction.Mul(b.speed()))
+}
+
+func (b *blaster) handleAttack() {
+	if b.attack == b.preAttackDelay() {
+		t := b.target()
+		if t != nil && b.withinRange(t) {
+			b.fire()
+		} else {
+			b.attack = 0
+			return
+		}
+	}
+	b.attack++
+	if b.attack > b.attackInterval() {
+		b.attack = 0
+	}
+}
