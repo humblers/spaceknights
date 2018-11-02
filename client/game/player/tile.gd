d5122ccee69f8996caf35bacd6d244c4e2cf55a0
Daeil Kim <kiidmale@gmail.com>
2018-11-02 15:58:33 +0900

    Apply changed tile resource to game scene

---
+++ client/game/player/tile.gd
@@ -0,0 +1,17 @@
+extends Control
+
+func ShowArea(is_unit):
+	visible = true
+	$Unit.visible = is_unit
+	$Spell.visible = not is_unit
+
+func Hide():
+	visible = false
+
+func OnKnightDead(team, side):
+	if team == "Blue":
+		$Unit.get_node("%s%s" % [team, side]).visible = true
+		$Unit/Deco.get_node("Knight%s" % side).visible = false
+	else:
+		$Unit.get_node("Extend%s" % side).visible = true
+		$Unit.get_node("Red%s" % side).visible = false
