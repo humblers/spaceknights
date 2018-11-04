3ef644a4b93b7661565c00aca72d0608115eca71
scottkim <kcwdetsu@gmail.com>
2018-11-05 02:02:58 +0900

    로비관련 전반 수정
    배틀페이지에 모선/배경/라이팅 적용
    리소스 전반 수정
    카드페이지 리소스 전반 수정
    카드 펼침메뉴창 사이즈좀 조절 부탁....

--- client/lobby/headup/headup.gd
+++ client/lobby/headup/headup.gd
@@ -3,15 +3,15 @@ extends CanvasLayer
 func _ready():
 	$Top/Config.connect("button_up", self, "show_config")
 	$Config/MarginContainer/Close.connect("button_up", self, "hide_config")
-	$Config/MarginContainer/Change.connect("button_up", self, "change_uid")
-	$Config/MarginContainer/Play.connect("button_up", self, "play_replay")
+	$Config/MarginContainer/Account/Change.connect("button_up", self, "change_uid")
+	$Config/MarginContainer/Replay/Play.connect("button_up", self, "play_replay")
 
 func invalidate():
 	$Top/Level.text = "%d" % (user.Level + 1)
 	$Top/Level/Exp.text = "%d/xxx" % user.Exp
 	$Top/Galacticoin.text = "%d" % user.Galacticoin
 	$Top/Dimensium.text = "%d" % user.Dimensium
-	$Config/MarginContainer/PID.text = user.PlatformId
+	$Config/MarginContainer/Account/PID.text = user.PlatformId
 
 func show_config():
 	$Config.popup()
@@ -20,7 +20,7 @@ func hide_config():
 	$Config.visible = false
 
 func change_uid():
-	var to = $Config/MarginContainer/PIDEdit.text
+	var to = $Config/MarginContainer/Account/PIDEdit.text
 	if to == "":
 		return
 	var config = ConfigFile.new()
