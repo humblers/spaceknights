cd9dc6fda5f11b0206b3b3c8778a21ca9ae6ba5c
JayB <jayb.ephemeral@icloud.com>
2018-11-11 10:06:47 +0900

    add flag parse line

--- cmd/game/main.go
+++ cmd/game/main.go
@@ -16,6 +16,7 @@ func main() {
 	flag.StringVar(&host, "host", "127.0.0.1", "game server host. default_host=127.0.0.1")
 	flag.StringVar(&port, "port", "9999", "game server port for client. default_port=9999")
 	flag.StringVar(&lport, "lport", "9989", "game server port for lobby. default_port=9989")
+	flag.Parse()
 
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt)
