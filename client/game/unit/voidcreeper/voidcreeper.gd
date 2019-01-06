5c642b8553122737a95dc632ad7851e0e549d46b
JayB <jayb.ephemeral@icloud.com>
2019-01-06 22:01:22 +0900

    voidcreeper

--- client/game/unit/voidcreeper/voidcreeper.gd
+++ client/game/unit/voidcreeper/voidcreeper.gd
@@ -6,6 +6,21 @@ var attack = 0
 func Init(id, level, posX, posY, game, player):
 	New(id, "voidcreeper", player.Team(), level, posX, posY, game)
 
+func TakeDamage(amount, damageType, attacker):
+	var alreadyDead = IsDead()
+	.TakeDamage(amount, damageType, attacker)
+	if not alreadyDead and IsDead():
+		for id in game.UnitIds():
+			var u = game.FindUnit(id)
+			if u.Team() == Team():
+				continue
+			var x = scalar.Sub(PositionX(), u.PositionX())
+			var y = scalar.Sub(PositionY(), u.PositionY())
+			var d = vector.LengthSquared(x, y)
+			var r = scalar.Add(u.Radius(), destroyRadius())
+			if d < scalar.Mul(r, r):
+				u.TakeDamage(destroyDamage(), destroyDamageType(), self)
+
 func Update():
 	.Update()
 	if freeze > 0:
@@ -59,10 +74,33 @@ func handleAttack():
 		look_at_pos(t.PositionX(), t.PositionY())
 	if attack == preAttackDelay():
 		if t != null and withinRange(t):
-			t.TakeDamage(attackDamage(), damageType(), self)
+			for id in game.UnitIds():
+				var u = game.FindUnit(id)
+				if u.Team() == Team():
+					continue
+				var x = scalar.Sub(t.PositionX(), u.PositionX())
+				var y = scalar.Sub(t.PositionY(), u.PositionY())
+				var d = vector.LengthSquared(x, y)
+				var r = scalar.Add(u.Radius(), damageRadius())
+				if d < scalar.Mul(r, r):
+					t.TakeDamage(attackDamage(), damageType(), self)
 		else:
 			attack = 0
 			return
 	attack += 1
 	if attack > attackInterval():
 		attack = 0
+
+func damageRadius():
+	var r = data.units[name_]["damageradius"]
+	return game.World().FromPixel(r)
+
+func destroyDamageType():
+	return data.units[name_]["destroydamagetype"]
+
+func destroyDamage():
+	return data.units[name_]["destroydamage"][level]
+
+func destroyRadius():
+	var r = data.units[name_]["destroyradius"]
+	return game.World().FromPixel(r)
