89af5b90fcfb6b1d7c7b0994431aa405bd46971d
DESKTOP-H7DFNL9\maru <hwangmaru@gmail.com>
2019-02-07 21:06:50 +0900

    상자 열기 씬 변경
    1. 카드 노드들 chest_card라는 서브씬을 공유하도록 구조 바꿈
    2. 상자 열기에서 카드가 보여줘야하는 정보가 카드페이지의 그것과 달라서 chest_card는 이전 씬을 고유화하여 수정을 가함. 따로 스크립트도 붙임
    3. 이름은 동적으로 잘 변하지만, 상자깔 때 얻는 카드 개수는 어디서 관장하는지 몰라서 못붙여놓음. 추후 붙여주길 바람.
    4. 레어리티별로 텍스트 색이 해당 레어리티 색으로 바뀌었으면 함

---
+++ client/lobby/page/battle/chest_card.gd
@@ -0,0 +1,46 @@
+extends BaseButton
+
+export(NodePath) onready var page_card = get_node(page_card)
+export(NodePath) onready var base = get_node(base)
+export(NodePath) onready var icon = get_node(icon)
+export(NodePath) onready var frame = get_node(frame)
+export(NodePath) onready var cost_label = get_node(cost_label)
+export(NodePath) onready var level_label = get_node(level_label)
+export(NodePath) onready var holding_progress = get_node(holding_progress)
+export(NodePath) onready var holding_label = get_node(holding_label)
+export(NodePath) onready var pressed_btn_guide = get_node(pressed_btn_guide)
+
+export(NodePath) onready var base_animation_player = get_node(base_animation_player)
+export(NodePath) onready var animation_player = get_node(animation_player)
+export(NodePath) onready var chest_card_name_label = get_node(chest_card_name_label)
+
+var card
+
+func _ready():
+	self.connect("button_down", self, "down")
+	self.connect("button_up", self, "up")
+
+func Invalidate(card):
+	self.card = card
+	self.visible = card != null
+	if card == null:
+		return
+	icon.texture = page_card.lobby.resource_manager.get_card_icon(card.Name)
+	frame.texture = page_card.lobby.resource_manager.get_card_frame(card.Type, card.Rarity)
+	cost_label.text = "%d" % (card.Cost / 1000)
+	level_label.text = "%02d" % (card.Level + data.Upgrade.dict.RelativeLvByRarity[card.Rarity] + 1)
+	var card_cost = data.Upgrade.CardCostNextLevel(card.Level)
+	holding_progress.max_value = card_cost
+	holding_progress.value = card.Holding
+	holding_label.text = "%d/%d" % [card.Holding, card_cost]
+	chest_card_name_label.SetText("ID_%s" % card.Name.to_upper())
+
+func down():
+	base_animation_player.play("down")
+
+func up():
+	base_animation_player.play_backwards("down")
+	page_card.set_pressed_card(self.card, pressed_btn_guide.global_position)
+
+func changed():
+	animation_player.play("changed")
