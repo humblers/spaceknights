5127506e2354f56b3681f5d22a0788a380c5d869
JayB <jayb.ephemeral@icloud.com>
2019-03-06 21:58:53 +0900

    add email authentication to desktop

--- client/lobby/script/firebase_auth.gd
+++ client/lobby/script/firebase_auth.gd
@@ -2,11 +2,12 @@ extends FirebaseAuth
 
 const GOOGLE_PROVIDER_ID = "google.com"
 const FACEBOOK_PROVIDER_ID = "facebook.com"
+const EMAIL_PASSWORD_PROVIDER_ID = "password"
 
 func _ready():
 	initialize(config.GOOGLE_PLAYSERVICE_JSON)
 
-func link_account(idp):
+func link_account(idp, setting_popup, arg1 = null, arg2 = null):
 	var token
 	match idp:
 		GOOGLE_PROVIDER_ID:
@@ -25,26 +26,34 @@ func link_account(idp):
 				return
 			token = res.access_token
 			link_with_facebook_access_token(token)
+		EMAIL_PASSWORD_PROVIDER_ID:
+			# arg1 == email, arg2 == password
+			assert(typeof(arg1) == TYPE_STRING)
+			assert(typeof(arg2) == TYPE_STRING)
+			link_with_email_and_password(arg1, arg2)
 		_:
 			global_object.lobby.http_manager.handle_error(idp)
 			return
 	var res = yield(self, "on_link_complete")
 	match res.auth_error:
 		kAuthErrorNone, kAuthErrorProviderAlreadyLinked:
+			setting_popup.Invalidate()
 			return
-		kAuthErrorCredentialAlreadyInUse:
+		kAuthErrorCredentialAlreadyInUse, kAuthErrorEmailAlreadyInUse:
 			var modal = global_object.lobby.hud.confirm_modal
 			modal.PopUp("ID_MODAL_CONFIRM_CHANGE_ACCOUNT")
 			var ok = yield(modal, "modal_confirmed")
 			if ok:
-				change_account(idp, token)
+				change_account(idp, token, arg1, arg2)
 
-func change_account(idp, token):
+func change_account(idp, token, arg1 = null, arg2 = null):
 	match idp:
 		GOOGLE_PROVIDER_ID:
 			sign_in_with_google_id_token(token)
 		FACEBOOK_PROVIDER_ID:
 			sign_in_with_facebook_access_token(token)
+		EMAIL_PASSWORD_PROVIDER_ID:
+			sign_in_with_email_and_password(arg1, arg2)
 		_:
 			global_object.lobby.http_manager.handle_error(idp)
 			return
