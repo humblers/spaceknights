df96b46a32bd67c04cd0e51a659efc6211ca3165
JayB <jayb.ephemeral@icloud.com>
2019-03-12 23:15:49 +0900

    random red deck
    fix bug

--- client/game/offline/tutor/tutor.gd
+++ client/game/offline/tutor/tutor.gd
@@ -13,10 +13,28 @@ func _enter_tree():
 	for player in cfg.Players:
 		if player.Team != "Red":
 			continue
-		var difficulty = player.Difficulty
-		for card in player.Deck:
-			var c = data.NewCard(card)
-			card.Level = max(0, difficulty - RelativeLvByRarity[c.Rarity])
+		var deck_length = len(player.Deck)
+		player.Deck = []
+		var sides = ["Center", "Left", "Right"]
+		var card_keys = data.cards.keys()
+		while len(player.Deck) < deck_length:
+			var side = sides.front()
+			var card_name = card_keys[randi() % len(card_keys)]
+			var card = {}
+			var want_card_type = data.SquireCard
+			if side != null:
+				card["Side"] = side
+				want_card_type = data.KnightCard
+			var info = data.cards[card_name]
+			if info.Type != want_card_type:
+				continue
+			card["Name"] = card_name
+			card["Level"] = max(0, player.Difficulty - RelativeLvByRarity[info.Rarity])
+			sides.erase(side)
+			card_keys.erase(card_name)
+			player.Deck.append(card)
+		player.Deck.shuffle()
+		print(player.Deck)
 	get_node("game").cfg = cfg
 
 func _physics_process(delta):
