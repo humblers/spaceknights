a346168964b7c434a017dc1c2012c208339932d1
JayB <jayb.ephemeral@icloud.com>
2019-05-02 21:10:33 +0900

    fix richtextlabel downsize bug

--- client/translation/dynamic_font_size_control.gd
+++ client/translation/dynamic_font_size_control.gd
@@ -2,45 +2,67 @@ extends Control
 
 const MIN_SIZE = 28
 
-var multiline = false
 var org_font_size
+var downsize_processing = false
+var downsize_booked = false
 
 func _ready():
+	var font = getFont()
+	assert(font is DynamicFont)
+	org_font_size = font.size
 	match self.get_class():
 		"Label":
 			assert(self.get("clip_text") == true)
+			self.add_font_override("font", font.duplicate())
+			SetText()
 		"RichTextLabel":
 			self.set("bbcode_enabled", true)
-	var font = getFont()
-	assert(font != null)
-	org_font_size = font.size
-	call_deferred("fontDownsizing")
+			self.add_font_override("normal_font", font.duplicate())
+			self.connect("visibility_changed", self, "SetText")
+			SetText()
 
-func SetText(id):
-	var property = "text"
-	if self.get_class() == "RichTextLabel":
-		property = "bbcode_text"
-	self.set(property, tr(id))
-	getFont().size = org_font_size
-	call_deferred("fontDownsizing")
-
-func fontDownsizing():
-	var font = getFont().duplicate()
+func SetText(id = null):
 	match self.get_class():
 		"Label":
+			if id == null:
+				id = self.get("text")
+			var text = tr(id)
+			self.set("text", text)
+			var font = getFont()
+			font.size = org_font_size
 			while font.size > MIN_SIZE:
-				var size = font.get_string_size(tr(self.get("text")))
+				var size = font.get_string_size(text)
 				if size.x <= self.rect_size.x && size.y <= self.rect_size.y:
 					break
 				font.size -= 1
-			self.add_font_override("font", font)
+			font.update_changes()
 		"RichTextLabel":
-			while font.size > MIN_SIZE:
-				if self.call("get_content_height") < self.rect_size.y:
-					break
-				font.size -= 1
-			self.add_font_override("normal_font", font)
-	font.update_changes()
+			if id == null:
+				id = self.get("bbcode_text")
+			self.set("bbcode_text", tr(id))
+			if not visible:
+				return
+			if downsize_processing:
+				downsize_booked = true
+				return
+			process()
+
+func process():
+	downsize_processing = true
+	self.set("visible_characters", 0)
+	var font = getFont()
+	font.size = org_font_size
+	while font.size > MIN_SIZE:
+		yield(get_tree(), "idle_frame")
+		if self.call("get_content_height") < self.rect_size.y:
+			break
+		font.size -= 1
+	self.set("visible_characters", -1)
+	if downsize_booked:
+		downsize_booked = false
+		process()
+		return
+	downsize_processing = false
 
 func getFont():
 	match self.get_class():
